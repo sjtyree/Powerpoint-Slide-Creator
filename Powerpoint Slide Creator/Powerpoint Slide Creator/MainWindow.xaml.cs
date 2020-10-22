54fef3cd5092d49d3e25f72fa04e9db050e1209e
Spencer James Tyree <sjtyree@hotmail.com>
2020-10-22 11:46:05 -0700

    Added powerpoint functionality

--- Powerpoint Slide Creator/Powerpoint Slide Creator/MainWindow.xaml.cs
+++ Powerpoint Slide Creator/Powerpoint Slide Creator/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Office.Core;
+using Microsoft.Office.Interop.PowerPoint;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,18 +20,77 @@ namespace Powerpoint_Slide_Creator
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
+    /// 
+    /// Powerpoint code taken from the following websites:
+    /// https://www.free-power-point-templates.com/articles/how-to-create-a-powerpoint-presentation-using-c-and-embed-a-picture-to-the-slide/
+    /// https://www.free-power-point-templates.com/articles/create-powerpoint-ppt-programmatically-using-c/
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Microsoft.Office.Interop.PowerPoint.Application pptApplication;
+        private Presentation pptPresentation;
+        private Microsoft.Office.Interop.PowerPoint.Slides slides;
+        private Microsoft.Office.Interop.PowerPoint._Slide slide;
+        private Microsoft.Office.Interop.PowerPoint.TextRange objText;
+        Microsoft.Office.Interop.PowerPoint.CustomLayout customLayout;
+        int slideNumber;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            //initialize powerpoint
+            pptApplication = new Microsoft.Office.Interop.PowerPoint.Application();
+
+            // Create the Presentation File
+            pptPresentation = pptApplication.Presentations.Add(MsoTriState.msoTrue);
+
+            customLayout = pptPresentation.SlideMaster.CustomLayouts[Microsoft.Office.Interop.PowerPoint.PpSlideLayout.ppLayoutText];
+
+            slideNumber = 1;
         }
 
         private void MainWindowButtonClick(object sender, RoutedEventArgs e)
         {
-            SearchResultsWindow newWindow = new SearchResultsWindow(this.MainWindowTitleText.Text, "");
-            newWindow.Show();
+            System.Windows.Documents.TextRange textRange = new System.Windows.Documents.TextRange(this.MainWindowSlideText.Document.ContentStart, this.MainWindowSlideText.Document.ContentEnd);
+            //MessageBox.Show(textRange.Text);
+            SearchResultsWindow newWindow = new SearchResultsWindow(this.MainWindowTitleText.Text, textRange.Text);
+            newWindow.ShowDialog();
+            ArrayList selectedUrls = newWindow.getSelectedUrls();
+            createSlide(selectedUrls);
         }
+
+        private void createSlide(ArrayList selectedUrls)
+        {
+            // Create new Slide
+            slides = pptPresentation.Slides;
+            slide = slides.AddSlide(slideNumber, customLayout);
+            //increment slide number
+            slideNumber++;
+            // Add title
+            objText = slide.Shapes[1].TextFrame.TextRange;
+            objText.Text = this.MainWindowTitleText.Text;
+            objText.Font.Name = "Arial";
+            objText.Font.Size = 32;
+
+            System.Windows.Documents.TextRange textRange = new System.Windows.Documents.TextRange(this.MainWindowSlideText.Document.ContentStart, this.MainWindowSlideText.Document.ContentEnd);
+
+            objText = slide.Shapes[2].TextFrame.TextRange;
+            objText.Text = textRange.Text;
+
+            Microsoft.Office.Interop.PowerPoint.Shape shape = slide.Shapes[2];
+            foreach (string url in selectedUrls)
+            {
+                slide.Shapes.AddPicture(url, Microsoft.Office.Core.MsoTriState.msoFalse, Microsoft.Office.Core.MsoTriState.msoTrue, shape.Left, shape.Top, shape.Width, shape.Height);
+            }
+        }
+
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            //do anything else that needs to be done before the program ends  
+            this.Close();
+        }
+
     }
 }
--- Powerpoint Slide Creator/Powerpoint Slide Creator/SearchResultsWindow.xaml.cs
+++ Powerpoint Slide Creator/Powerpoint Slide Creator/SearchResultsWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Office.Core;
+using Microsoft.Office.Interop.PowerPoint;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,6 +25,10 @@ namespace Powerpoint_Slide_Creator
     /// </summary>
     public partial class SearchResultsWindow : Window
     {
+        private string titleText;
+        private string slideText;
+        private ArrayList urls;
+        private ArrayList selected_urls;
         public SearchResultsWindow()
         {
             InitializeComponent();
@@ -31,36 +37,42 @@ namespace Powerpoint_Slide_Creator
         public SearchResultsWindow(string TitleText, string SlideText)
         {
             InitializeComponent();
-            ArrayList urls = new ArrayList();
-            getImageURLs(urls, TitleText, SlideText);
-            addImagesToListBox(urls);
+            this.titleText = TitleText;
+            this.slideText = SlideText;
+            
+            //initialize arrays
+            urls = new ArrayList();
+            selected_urls = new ArrayList();
+
+            getImageURLs(TitleText, SlideText);
+            addImagesToListBox();
 
         }
 
-        private void getImageURLs(ArrayList urls, string titleText, string slideText)
+        private void getImageURLs(string titleText, string slideText)
         {
-            //HttpClient testClient = new HttpClient();
+
             //create web client
             WebClient googleImages = new WebClient();
-            Regex googleRegex = new Regex(@"src=""https://[^""]*""", RegexOptions.Compiled | RegexOptions.IgnoreCase);//\\""[^""]*""");
+            //This regex searches for image urls in the html from google
+            Regex googleRegex = new Regex(@"src=""https://[^""]*""", RegexOptions.Compiled | RegexOptions.IgnoreCase);
             //get google html for image search
             string html = googleImages.DownloadString("https://www.google.com/search?tbm=isch&q=" + titleText + "+" + slideText);
-            //MessageBox.Show(html);
             MatchCollection googleMatches = googleRegex.Matches(html);
             
             //MessageBox.Show("" + googleMatches.Count);
             foreach (Match m in googleMatches)
             {
                 //add the match to the arraylist
-                urls.Add(m.Value);
+                this.urls.Add(m.Value);
             }
 
         }
 
-        private void addImagesToListBox(ArrayList urls)
+        private void addImagesToListBox()
         {
             int iterator;
-            foreach (string x in urls) //(int x = 0; x < urls.Count; x++)
+            foreach (string x in this.urls) //(int x = 0; x < urls.Count; x++)
             {
                 iterator = 1;
                 Console.WriteLine(x);
@@ -71,7 +83,6 @@ namespace Powerpoint_Slide_Creator
                 googleImage.Name = "Image" + iterator;
                 googleImage.Source = new BitmapImage(new Uri(x.Substring(5, x.Length - 6)));
                 this.Image_List_Box.Items.Add(googleImage);
-
                 //create the corresponding checkbox and add it to the listbox
                 CheckBox listCheckBox = new CheckBox();
                 listCheckBox.Name = "Checkbox" + iterator;
@@ -83,6 +94,32 @@ namespace Powerpoint_Slide_Creator
             //Image mechonis = new Image();
             //mechonis.Source = new BitmapImage(new Uri(@"C:\Users\Spencer\source\repos\SEH-Interview-Test\Powerpoint Slide Creator\Powerpoint Slide Creator\Test Images\mechonis.jpg")); 
             //this.Image_List_Box.Items.Add(mechonis);
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+            for (int x=1; x<this.Image_List_Box.Items.Count; x=x+2) //ListBoxItem item in this.Image_List_Box.Items)
+            {
+                CheckBox checkbox = (CheckBox)this.Image_List_Box.Items.GetItemAt(x);
+                if (checkbox.IsChecked == true)
+                {
+                    //grab the image from the collection
+                    Image image = (Image)this.Image_List_Box.Items.GetItemAt(x - 1);
+                    //add image url to selected urls arraylist
+                    this.selected_urls.Add(image.Source.ToString());
+                }
+            }
+            this.Close();
+        }
+        public ArrayList getSelectedUrls()
+        {
+            return this.selected_urls;
         }
     }
 }
